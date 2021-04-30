99495d082e0aaed1438aa095f9b3b28b407a414a
lmrabiah <lm.alrabiah@gmail.com>
2021-04-30 09:50:59 +0300

    D

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const db = require("./db/models");
+const passport = require("passport");
+const { localStrategy, jwtStrategy } = require("./middleware/passport");
+const app = express();
+const bodyParser = require("body-parser");
+// const path = require("path");
+const userRoutes = require("./routes/users");
+//middlewere
+// console.log("__dirname ", __dirname);
+
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(passport.initialize());
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+////Route
+app.use(userRoutes);
+
+// if i put wrong url for example: localhost:8000/tripssss
+app.use((req, res, next) => {
+  console.log("Path dosn't exist");
+  res.status(404).json({ message: "Path not found" });
+});
+
+//all errors (error handle middle ware)
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status ?? 500);
+  res.json({ message: err.message ?? "internal server error" });
+});
+
+const run = async () => {
+  try {
+    await db.sequelize.sync({ alter: true });
+    console.log("Connection to the database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const bcrypt = require("bcrypt");
+const { User } = require("../db/models");
+const jwt = require("jsonwebtoken");
+const { JWT_EXPIRATION_MS, JWT_SECRET } = require("../config/keys");
+
+exports.signup = async (req, res, next) => {
+  const { password } = req.body;
+  const saltRounds = 10;
+  try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    console.log("exports.signup -> hashedPassword", hashedPassword);
+    req.body.password = hashedPassword;
+    const newUser = await User.create(req.body);
+    const payload = {
+      id: newUser.id,
+      username: newUser.username,
+      massage: "wooow",
+      email: newUser.email,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      exp: Date.now() + JWT_EXPIRATION_MS,
+    };
+    const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+    req.body.userId = newUser.id;
+
+    res.status(201).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  try {
+    const { user } = req;
+    const payload = {
+      id: user.id,
+      username: user.username,
+      exp: Date.now() + 900000,
+    };
+
+    const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+
+    res.json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
